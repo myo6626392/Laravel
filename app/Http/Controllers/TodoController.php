c2c2e3bf6ccce9ec89abff9d3a8fcb7390723a4b
myo6626392 <myo6626392@gmail.com>
2025-10-16 21:54:32 +0630

    updated

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -5,8 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
+//index function (main list)
 {
-    public function index(){
-        return view('todos.index');
+    public function index()
+    {
+        //In memory data (mock data)
+        $todos = [
+            ['id' => 1, 'title' => 'Buy groceries', 'completed' => false],
+            ['id' => 2, 'title' => 'Clean the house', 'completed' => true],
+            ['id' => 3, 'title' => 'Pay Bills', 'completed' => false],
+            ['id' => 4, 'title' => 'Reserve the table', 'completed' => true],
+        ];
+        return  view('todos.index', [
+            'todos' => $todos
+        ]);
     }
 }
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -2,7 +2,22 @@
 @section('title','Todo Page')
 @section('content')
 
-<h1>Todo Page</h1>
+<h1>Todo Lists</h1>
+<ul>
+    @foreach ($todos as $todo )
+    <li>
+        {{ $todo['title'] }} - {{ $todo['completed']? 'Completed' : 'Not Completed' }}
+        <a href="{{ route('todos.edit',$todo['id']) }}" class="btn btn-primary">Edit</a>
+        <form action="{{ route('todos.destroy',$todo['id']) }} " method='POST' style="display: inline";>
+            @csrf
+            @method('DELETE')
+         {{-- hidden input field with method DELETE overwrite --}}
+         <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this todo?')">Delete</button>
+        </form>
+    </li>
+
+    @endforeach
+</ul>
 
 @endsection
 
